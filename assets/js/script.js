7066976009442dc6acb5d0ccad2727237f5ee27c
Josie Zobel <159351625+jobeans24@users.noreply.github.com>
2024-04-02 19:29:16 -0400

    Creating a modal for the book search and preparing the java for the apis and bootstrap styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,12 @@
 function openModal() {
     document.getElementById("modal").style.display = "block";
 }
+
+function openModal2() {
+    document.getElementById("search-modal").style.display = "block";
+}
+
+function closeModal() {
+    document.getElementById("modal").style.display = "none";
+}
+
