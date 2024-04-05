0d14ce7387f6f106a65b4677a998d480feb080d0
Josie Zobel <159351625+jobeans24@users.noreply.github.com>
2024-04-04 21:07:25 -0400

    creating to dos on the

---
+++ assets/js/review.js
@@ -0,0 +1,12 @@
+// To Do: Declare global variables
+
+// To Do: Link Back button to index.html
+
+// To Do: Retrieve review data from local storage submitted the review modal   
+
+// To Do: display review data in the book review format
+
+// To do: add event listener for review form submission (form can be copied from index.html for the modal here if needed)
+
+// Possible To Do: Add Reddit API to share reddit reviews
+
