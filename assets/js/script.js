481665111afe899c461cd2bd8e87677fb67d848a
Josie Zobel <159351625+jobeans24@users.noreply.github.com>
2024-04-06 18:29:09 -0400

    Adding the NYT best sellers API and the container for the information to be displayed. Added stylizations to teh buttons on the screens

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,11 +13,52 @@ previousResults.forEach(result => {
         <div class="card-body">
             <h5 class="card-title">${result.title}</h5>
             <p class="card-text">${result.author}</p>
-            <p class="card-text">${result.genre}</p>
+            
         </div>
     `;
     previousResultsContainer.appendChild(card);
 });
+
+// To Do : Add NYT Best Sellers API fetch function
+// Function to fetch NYT Best Sellers API data
+function getBestSellersData() {
+    // API endpoint
+    const bestSellersAPI = 'https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=AFYgXoe5pmVDuEA0fr01nWXwxIu38wYX';
+    console.log(bestSellersAPI);
+
+    // Fetch data from the API
+    fetch(bestSellersAPI)
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
+                    console.log(data);
+                    // To Do: Make use of data
+
+                    // Display books in the nyt-container
+                    const nytContainer = document.getElementById('nyt-container');
+                    data.results.books.forEach(book => {
+                        const card = document.createElement('div');
+                        card.classList.add('card');
+                        card.innerHTML = `
+                            <div class="card-body">
+                                <h5 class="card-title">${book.title}</h5>
+                                <p class="card-text">Author: ${book.author}</p>
+                                <p class="card-text">Genre: ${book.genre}</p>
+                            </div>
+                        `;
+                        nytContainer.appendChild(card);
+                    });
+                    
+                });
+            } else {
+                alert('Error: ' + response.statusText);
+            }
+        });
+}
+
+// Call the getBestSellersData function to fetch data from the NYT Best Sellers API
+getBestSellersData();
+
 
 //To Do: Link review button to review.html
 window.onload = function() {
