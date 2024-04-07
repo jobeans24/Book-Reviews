e049056e79d97da705e618125f822dbd50971076
dwalley606 <dwalley606@gmail.com>
2024-04-07 13:33:01 -0400

    Fixes for formatting, deleted duplicated function in script.js

--- assets/js/review.js
+++ assets/js/review.js
@@ -2,7 +2,22 @@
 
 // To Do: Link Back button to index.html
 
-// To Do: Retrieve review data from local storage submitted the review modal   
+
+
+// Retrieve review data from local storage
+function getReviewData() {
+    let reviewData = localStorage.getItem("reviews");
+    if (reviewData) {
+        return JSON.parse(reviewData);
+    } else {
+        return [];
+    }
+}
+
+// Call the getReviewData function to retrieve the review data
+let reviews = getReviewData();
+console.log(reviews);
+
 
 // To Do: display review data in the book review format
 
